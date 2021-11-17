dacf121a324dda18468c2872be64a958fc7cd80c
Bagas Wahyu Hidayah <bagaswh01@gmail.com>
2021-11-17 12:29:49 +0700

    update

--- common.sh
+++ common.sh
@@ -4,13 +4,16 @@ set -e
 sudo apt update
 
 # create swap
-sudo swapoff -a
-sudo fallocate -l 2G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-sudo cat <<EOF >>/etc/fstab
+if free | awk '/^Swap:/ {exit !$2}'; then
+    echo "swap exists, skipping swap creation..."
+else
+    sudo fallocate -l 2G /swapfile
+    sudo chmod 600 /swapfile
+    sudo mkswap /swapfile
+    sudo swapon /swapfile
+    sudo cat <<EOF >>/etc/fstab
 /swapfile swap swap defaults 0 0
 EOF
-sudo swapon /swapfile
+fi
+
 sudo swapon --show
