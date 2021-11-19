92199fb694b175d5db5a31cda67d953035489dd7
Bagas Wahyu Hidayah <bagaswh@protonmail.com>
2021-11-19 11:11:23 +0700

    push

--- start.sh
+++ start.sh
@@ -8,6 +8,10 @@ ls
 apt update
 apt install unzip curl -y
 
-./common.sh
+wget https://raw.githubusercontent.com/bagaswh/scripts-for-testing-azure-vm-extension/master/common.sh -O common.sh
+wget https://raw.githubusercontent.com/bagaswh/scripts-for-testing-azure-vm-extension/master/automate-litespeed.sh -O automate-litespeed.sh
+wget https://raw.githubusercontent.com/bagaswh/scripts-for-testing-azure-vm-extension/master/automate-nginx.sh -O automate-nginx.sh
+wget https://raw.githubusercontent.com/bagaswh/scripts-for-testing-azure-vm-extension/master/automate-nodejs.sh -O automate-nodejs.sh
 
+./common.sh
 ./automate-litespeed.sh
