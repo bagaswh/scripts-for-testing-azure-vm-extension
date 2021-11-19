c066385507f9cfcf78dfee09f9cfe79288258088
Bagas Wahyu Hidayah <bagaswh@protonmail.com>
2021-11-19 12:33:42 +0700

    OH MY GOOOOOOOOOOOOOOOOOOODDDDDDDDDDDDDDD

--- automate-litespeed.sh
+++ automate-litespeed.sh
@@ -1,7 +1,12 @@
 set -e
 
-wget -d https://raw.githubusercontent.com/bagaswh/scripts-for-testing-azure-vm-extension/master/assets/litespeed-confs/httpd_config.conf -O httpd_config.conf
+wget -q https://raw.githubusercontent.com/bagaswh/scripts-for-testing-azure-vm-extension/master/assets/litespeed-confs/httpd_config.conf -O httpd_config.conf
+echo 'downloade httpd_config.conf'
+cat httpd_config.conf
+
 wget -q https://raw.githubusercontent.com/bagaswh/scripts-for-testing-azure-vm-extension/master/assets/litespeed-confs/vhconf.conf -O vhconf.conf
+echo 'downloaded vhconf.conf'
+cat vhconf.conf
 
 wget -O - http://rpms.litespeedtech.com/debian/enable_lst_debian_repo.sh | sudo bash
 apt install -y openlitespeed
@@ -27,7 +32,7 @@ mkdir -p $vhost_log
 chown www-data:www-data -R $vhost_log
 chmod 640 -R $vhost_log
 
-export HTTPD_CONFIG_PATH=./httpd_config.conf
+export HTTPD_CONFIG_PATH=/usr/local/lsws/conf/httpd_config.conf
 
 # do:
 # - set user permission for LiteSpeed and ext apps
@@ -36,10 +41,10 @@ export HTTPD_CONFIG_PATH=./httpd_config.conf
 cat ./httpd_config.conf >$HTTPD_CONFIG_PATH
 
 vhost_config_dir=/usr/local/lsws/conf/vhosts/main/
-export VHOST_CONFIG_PATH=$vhost_config_dir/vhconf.conf
-
 mkdir -p $vhost_config_dir
 chown lsadm:www-data $vhost_config_dir
+
+export VHOST_CONFIG_PATH=$vhost_config_dir/vhconf.conf
 touch $VHOST_CONFIG_PATH
 chmod 750 -R $vhost_config_dir
 
