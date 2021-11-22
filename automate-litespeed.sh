df5a3dda3e59d3c46f4abe580489ae6dedcaa79c
Bagas Wahyu Hidayah <bagaswh@protonmail.com>
2021-11-22 15:26:19 +0700

    update

--- automate-litespeed.sh
+++ automate-litespeed.sh
@@ -11,27 +11,28 @@ cat vhconf.conf
 wget -O - http://rpms.litespeedtech.com/debian/enable_lst_debian_repo.sh | sudo bash
 apt install -y openlitespeed
 
+sudo apt install lsphp74-* -y
+sudo apt install lsphp80-* -y
+
 # setup default vhost directory
-vhost_root=/var/www/lsws_vhosts/main
+vhost_dir=/var/www/lsws_vhosts
+vhost_root=$vhost_dir/main
 mkdir -p $vhost_root
-chown azureuser:www-data $vhost_root
-chmod 750 -R $vhost_root
+chown azureuser:www-data -R $vhost_dir
+chmod 750 -R $vhost_dir
 
 # lsws log directory
 server_log=/var/log/lsws
-server_access_log=$server_log/access.log
-server_error_log=$server_log/error.log
 mkdir -p $server_log
 chown www-data:www-data -R $server_log
 chmod 640 -R $server_log
 
 # default vhost log directory
-vhost_log=/var/log/lsws/vhosts/main
-vhost_access_log=$vhost_log/access.log
-vhost_error_log=$vhost_log/error.log
+vhosts_log_dir=/var/log/lsws/vhosts
+vhost_log=$vhosts_log_dir/main
 mkdir -p $vhost_log
-chown www-data:www-data -R $vhost_log
-chmod 640 -R $vhost_log
+chown www-data:www-data -R $vhosts_log_dir
+chmod 640 -R $vhosts_log_dir
 
 export HTTPD_CONFIG_PATH=/usr/local/lsws/conf/httpd_config.conf
 
@@ -44,16 +45,10 @@ cat ./httpd_config.conf >$HTTPD_CONFIG_PATH
 vhost_config_dir=/usr/local/lsws/conf/vhosts/main/
 mkdir -p $vhost_config_dir
 chown lsadm:www-data $vhost_config_dir
+chmod 750 -R $vhost_config_dir
 
 export VHOST_CONFIG_PATH=$vhost_config_dir/vhconf.conf
 touch $VHOST_CONFIG_PATH
-chmod 750 -R $vhost_config_dir
-
-# apply config change
-service lsws restart
-
-# reinstall OLS to refresh file ownership
-apt install --reinstall -y openlitespeed
 
 # vhost setup
 # do:
@@ -61,7 +56,7 @@ apt install --reinstall -y openlitespeed
 # - block sensitive files
 cat ./vhconf.conf >$VHOST_CONFIG_PATH
 
-service lsws restart
+# reinstall OLS to refresh file ownership
+apt-get -y install --reinstall openlitespeed
 
-sudo apt install lsphp74-* -y
-sudo apt install lsphp80-* -y
+service lsws restart
