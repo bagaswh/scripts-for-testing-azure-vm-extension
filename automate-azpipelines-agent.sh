70dd46ce8d0512acb6cd23c052d3013241c629b4
Bagas Wahyu Hidayah <bagaswh@protonmail.com>
2021-11-22 14:36:48 +0700

    update

--- automate-azpipelines-agent.sh
+++ automate-azpipelines-agent.sh
@@ -1,3 +1,5 @@
-cp assets/az-pipelines-agent/deploy.sh /home/azureuser
+wget -q $base_url/assets/az-pipelines-agent/deploy.sh -O az-pipelines-agent-deploy.sh
+
+cp az-pipelines-agent-deploy.sh /home/azureuser/deploy.sh
 chown azureuser:azureuser /home/azureuser/deploy.sh
 chmod +x /home/azureuser/deploy.sh
